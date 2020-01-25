ee4f30005314491aa873526abac49bf489b9a051
Eyal Lev <eyal18yaffa03@gmail.com>
2020-01-26 00:17:17 +0200

    before talking with back4app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom'
-//import Parse from 'parse'
-
+import Parse from 'parse'
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+Parse.initialize(
+    'HSnCD0iQZOWlFq55YJw9zeI3hkrd9QDK3jImQlKY', // This is your Application ID
+    '4bGuPBpfk0sklFk9Dy2HzOqzy2xq0rahKa6s1Ox3' // This is your Javascript key
+);
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/model/TanantsModel.js
+++ src/model/TanantsModel.js
@@ -1,7 +1,7 @@
 export default class TanantsModel {
     constructor(parseTanants) {
-        this.id = parseTanants.get("apartment");
-        this.lname = parseTanants.get("lname");
+        this.id = parseTanants.get("objectId");
+        this.lname = parseTanants.get("username");
         this.email = parseTanants.get("email");
         this.apartment = parseTanants.get("apartment");
         this.isCommitteeMember = parseTanants.get("isCommitteeMember");
